7942a2c854aa7ed59e704f4c3ea87e2567e3e65d
swgaete <swgaete@uc.cl>
2016-03-16 00:18:07 -0300

    asd

--- Tarea1/src/JavaApplication1.java
+++ Tarea1/src/JavaApplication1.java
@@ -3,13 +3,14 @@ import java.security.MessageDigest;
 import java.util.Scanner;
 
 public class JavaApplication1 {
+	static boolean SonIguales;
 	 public static void main(String[] args)throws Exception
 	    {
 	        
 	        Scanner sc = new Scanner(System.in);
 	        String mensaje;
 	        System.out.print("Introduzca el mensaje: ");
-	        mensaje = sc.nextLine();
+	        mensaje = sc.nextLine();	        
 	    	String hash;
 	        System.out.print("Introduzca el hash: ");	
 	        hash = sc.nextLine();	
@@ -19,36 +20,41 @@ public class JavaApplication1 {
 	        
 	        byte byteData[] = md.digest();
 	 
-	        //convert the byte to hex format method 1
 	        StringBuffer sb = new StringBuffer();
 	        for (int i = 0; i < byteData.length; i++) {
 	         sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
 	        }
-	        
-	        //convert the byte to hex format method 2
 	        StringBuffer hexString = new StringBuffer();
 	    	for (int i=0;i<byteData.length;i++) {
 	    		String hex=Integer.toHexString(0xff & byteData[i]);
 	   	     	if(hex.length()==1) hexString.append('0');
 	   	     	hexString.append(hex);
 	    	}
-	    	
-	    	
 	        String FinalHash = hexString.toString();
 	         if(hash.toLowerCase().equalsIgnoreCase(FinalHash)){         
 	            System.out.println("Valido: True");
 	            System.out.println("Mensaje: "+mensaje);
+	            SonIguales(true);
 	         
 	          
 	        }else{
 	            System.out.println("Valido: False"); 
 	            System.out.println("Mensaje : " +mensaje);
 	            System.out.println("Formato real calculado Hex : " + hexString.toString());
-	            
+	            SonIguales(false);
 	           
 	        }
 	         
 	         
+	    }
+	 
+	 public static boolean SonIguales(boolean year) {
+	        if (year)
+	        	return  true;
+
+	        else
+	        	return false;
+
 	    }
 	 
 	
